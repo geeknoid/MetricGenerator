86ab5fd91bc6339e9266040679f7501dc37b3177
Martin Taillefer <mataille@microsoft.com>
2021-02-09 17:01:03 -0800

    Initial checkin

---
+++ MetricGenerator/All.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace MetricGenerator
+{
+    /***** Platform types defined in R9 *****/
+
+    // indicates a field is declaring a label name which triggers code gen
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class LabelNamesAttribute : Attribute
+    {
+    }
+
+    // indicates a method is a metric reporter and triggers code gen
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class MetricAttribute : Attribute
+    {
+    }
+
+    // a place where to send metrics.
+    public interface IMetricRecorder
+    {
+        void Record(string name, int value, Span<string> labelNames, Span<string> labelValues);
+    }
+
+    // a type which holds a bunch of ordered label names
+    public class LabelNames
+    {
+        public LabelNames(params string[] names)
+        {
+            // not implemented
+        }
+
+        public LabelNames(LabelNames ancestors, params string[] names)
+        {
+            // not implemented
+        }
+
+        // return the set of label names, all ancestors are flattened into a single list
+        public Span<string> AllNames => new string[0];
+    }
+
+    /***** Example of stuff the service developer writes */
+
+    public static partial class Metrics
+    {
+        // Defines a base set of ambient labels
+        [LabelNames]
+        private static readonly LabelNames _ambientLabels = new LabelNames("ClusterId", "PodId");
+
+        // Defines a set of ops labels, available in a nested context
+        [LabelNames]
+        private static readonly LabelNames _opLabels = new LabelNames(_ambientLabels, "Operation");
+
+        // Defines the OperationCount metric and the labels expected with it
+        [Metric]
+        public static partial void OperationCount(IMetricRecorder recorder, int value, OpLabels labels);
+    }
+
+    /***** Stuff we auto-generate based on what the developer wrote above *****/
+
+    public static partial class Metrics
+    {
+        public static partial void OperationCount(IMetricRecorder recorder, int value, OpLabels labels)
+        {
+            // ideally this would be a span allocated on the stack, but this is not allowed for strings. So we'll eventually get this array from a pool
+            var values = new string[3];
+            values[0] = labels.AmbientLabels.ClusterId;
+            values[1] = labels.AmbientLabels.PodId;
+            values[2] = labels.Operation;
+            recorder.Record("OperationCount", value, _opLabels.AllNames, values);
+        }
+
+        public readonly struct AmbientLabels
+        {
+            public readonly string ClusterId;
+            public readonly string PodId;
+
+            public AmbientLabels(string clusterId, string podId)
+            {
+                ClusterId = clusterId;
+                PodId = podId;
+            }
+        }
+
+        public readonly struct OpLabels
+        {
+            public readonly AmbientLabels AmbientLabels;
+            public readonly string Operation;
+
+            public OpLabels(AmbientLabels ambientLabels, string operation)
+            {
+                AmbientLabels = ambientLabels;
+                Operation = operation;
+            }
+        }
+    }
+
+    /***** Example usage of the metric stuff in a service *****/
+
+    public class MyService
+    {
+        private readonly IMetricRecorder _recorder; // get this from DI
+
+        public void APIDispatch()
+        {
+            // create the ambient labels
+            var ambientLabels = new Metrics.AmbientLabels("123", "456");
+
+            DeleteObject(ambientLabels);
+        }
+
+        public void DeleteObject(Metrics.AmbientLabels ambientLabels)
+        {
+            // create the specialized labels
+            var opLabels = new Metrics.OpLabels(ambientLabels, "DeleteObject");
+
+            // increment the metric, supplying all the requisite labels
+            Metrics.OperationCount(_recorder, 1, opLabels);
+
+            // The above will add 1 to the metric "OperationCount" to the cell identified by the labels
+            //   ClusterId = "123"
+            //   PodId = "456"
+            //   Operation = "DeleteObject"
+        }
+    }
+}
---
+++ MetricGenerator/README.md
@@ -0,0 +1,52 @@
+﻿# Metrics
+
+Metrics are recorded at runtime and have 3 core components:
+
+    * Name
+    * Value
+    * Labels
+
+where labels are a set of name/value pairs where both name and value are strings.
+When you want to record a metric, you need to supply all three value. The labels serve
+to define a unique counter associated with the given metric. Different labels means 
+a different counter.
+
+The set of labels associated with a metric can come from different places in an executing
+program:
+
+    * Ambient. This are immutable values for the life of the process. Perhaps this includes the 
+      pod id, the cluster id, the region id, etc.
+
+    * Contextual. This depends on the operation being performed. For example, maybe some extra labels
+      are created to represent the identity of a particular caller to the service.
+
+    * Scoped. This depends on the specific operation being performed. If you're reading a resource on behalf of a user, the labels might include the resource id.
+
+So as execution progresses, more label values become available. At some point, all the labels have been accumulated and its time to actually produce a metric
+using all those labels to identity the particular counter to manipulate.
+
+# The Model
+
+What we are defining here is a model to make it easy to declare the known label names used in a program, along with the set of known metrics it
+produces:
+
+* You define label names as either absolute, or as extensions over an existing set of label names. This captures the nesting model where some label values are
+known at certain places in the code, and more values become available as execution progresses.
+
+* You define the metrics you program reports along with the set of labels that metric uses to identify its counters. In order to emit that metric, code will
+be required to acquire all the stipulated metric values.
+
+Once you've defined these few pieces of state, code generation kicks in and produces several artifacts. The primary artifact of concern to the 
+developer is the metric recording function which takes a metric recorder (where metrics are pushed), along with a set of label names and label values.
+
+The motivation for this approach is that:
+
+* It provides a declarative model to define metrics used in a program.
+
+* It provides strongly-typed methods for recording metrics, which deliver IntelliSense and prevent recording metrics 
+which accidentally skip important labels.
+
+* It is efficient, avoid work that may otherwise be needed and minimizing allocations. For example, it is necessary to
+ensure label names are unique within a metric, which would normally requiring explicit sorting and dedupping of names
+every time a metric is reported. In this model however, this can be handled at initialization time instead at runtime.
+ 
